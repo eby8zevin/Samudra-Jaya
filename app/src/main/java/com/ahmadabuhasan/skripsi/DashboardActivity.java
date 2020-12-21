17aefe13781ee056694761dc189949e90125e275
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-22 04:13:11 +0700

    Skripsi

--- app/src/main/java/com/ahmadabuhasan/skripsi/DashboardActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/DashboardActivity.java
@@ -1,14 +1,30 @@
 package com.ahmadabuhasan.skripsi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    CardView cardViewKaca;
+    CardView cardViewPigura;
+    CardView cardViewKasir;
+    CardView cardViewData;
+    CardView cardViewPrint;
+    CardView cardViewSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        cardViewKaca = findViewById(R.id.card_kaca);
+        cardViewPigura = findViewById(R.id.card_pigura);
+        cardViewKasir = findViewById(R.id.card_kasir);
+        cardViewData = findViewById(R.id.card_data);
+        cardViewPrint = findViewById(R.id.card_print);
+        cardViewSettings = findViewById(R.id.card_settings);
+
     }
 }
