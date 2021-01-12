09e5ca22e937d03ba566976422f04fcb87d8467b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-12 17:54:18 +0700

    add ApiClient

---
+++ app/src/main/java/com/ahmadabuhasan/samudrajaya/ApiClient.java
@@ -0,0 +1,9 @@
+package com.ahmadabuhasan.samudrajaya;
+
+public class ApiClient {
+
+    public static final String URL_CREATE = "";
+    public static final String URL_READ = "http://IP_AddressServer/sj/read.php";
+    public static final String URL_EDIT = "";
+    public static final String URL_DELETE = "";
+}
